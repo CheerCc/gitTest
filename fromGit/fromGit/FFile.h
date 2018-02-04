bafe65fff159b6ae7b63a26f73af755e8580a603
蔡和丰 <249582589@qq.com>
2018-02-04 17:19:45 +0800

    c

---
+++ fromGit/fromGit/FFile.h
@@ -0,0 +1,13 @@
+//
+//  FFile.h
+//  fromGit
+//
+//  Created by 蔡和丰 on 2018/2/4.
+//  Copyright © 2018年 Cc_Alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FFile : NSObject
+
+@end
