67f98d7035449acfd1990f09c3152ad8217f96fd
蔡和丰 <249582589@qq.com>
2018-02-04 17:13:37 +0800

    commit

---
+++ fromGit/fromGit/B3File.h
@@ -0,0 +1,13 @@
+//
+//  B3File.h
+//  fromGit
+//
+//  Created by 蔡和丰 on 2018/2/4.
+//  Copyright © 2018年 Cc_Alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface B3File : NSObject
+
+@end
---
+++ fromGit/fromGit/B3File.m
@@ -0,0 +1,13 @@
+//
+//  B3File.m
+//  fromGit
+//
+//  Created by 蔡和丰 on 2018/2/4.
+//  Copyright © 2018年 Cc_Alex. All rights reserved.
+//
+
+#import "B3File.h"
+
+@implementation B3File
+
+@end
